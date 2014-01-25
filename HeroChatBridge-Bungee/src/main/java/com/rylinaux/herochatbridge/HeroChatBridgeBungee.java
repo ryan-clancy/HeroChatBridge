415db259ecfc8dbc5fbd10236870024ac0b6fb6a
rylinaux <rylinaux@gmail.com>
2014-01-24 21:38:41 -0400

    Add onDisable method. Add override annotations.

--- HeroChatBridge-Bungee/src/main/java/com/rylinaux/herochatbridge/HeroChatBridgeBungee.java
+++ HeroChatBridge-Bungee/src/main/java/com/rylinaux/herochatbridge/HeroChatBridgeBungee.java
@@ -1,14 +1,21 @@
 package com.rylinaux.herochatbridge;
 
-import net.md_5.bungee.api.plugin.Plugin;
-
 import com.rylinaux.herochatbridge.listeners.PluginMessageListener;
+
+import net.md_5.bungee.api.plugin.Plugin;
 
 public class HeroChatBridgeBungee extends Plugin {
 
+    @Override
     public void onEnable() {
         this.getProxy().registerChannel("HeroChatBridge");
         this.getProxy().getPluginManager().registerListener(this, new PluginMessageListener(this));
+    }
+
+    @Override
+    public void onDisable() {
+        this.getProxy().unregisterChannel("HeroChatBridge");
+        this.getProxy().getPluginManager().unregisterListeners(this);
     }
 
 }
