357bbad676a673aa1a1a351c09fb113d6a682a77
rylinaux <rylinaux@gmail.com>
2014-02-16 20:43:29 -0400

    Formatting

--- HeroChatBridge-Bukkit/src/main/java/com/rylinaux/herochatbridge/bukkit/utilities/MessageFormatter.java
+++ HeroChatBridge-Bukkit/src/main/java/com/rylinaux/herochatbridge/bukkit/utilities/MessageFormatter.java
@@ -7,19 +7,19 @@ import org.bukkit.ChatColor;
 public class MessageFormatter {
 
     public static String format(String sender, String message, String fromServer, String world, Channel channel) {
-        String formatted = channel.getFormatSupplier().getStandardFormat();
-        formatted = formatted.replace("{name}", channel.getName());
-        formatted = formatted.replace("{nick}", channel.getNick() + "|" + fromServer);
-        formatted = formatted.replace("{color}", channel.getColor().toString());
-        formatted = formatted.replace("{msg}", message);
-        formatted = formatted.replace("{sender}", VaultUtil.getPlayerName(sender));
-        formatted = formatted.replace("{plainsender}", sender);
-        formatted = formatted.replace("{world}", world);
-        formatted = formatted.replace("{prefix}", VaultUtil.getPlayerPrefix(sender));
-        formatted = formatted.replace("{suffix}", VaultUtil.getPlayerSuffix(sender));
-        formatted = formatted.replace("{group}", VaultUtil.getGroup(sender));
-        formatted = formatted.replace("{groupprefix}", VaultUtil.getGroupPrefix(sender));
-        formatted = formatted.replace("{groupsuffix}", VaultUtil.getGroupSuffix(sender));
+        String formatted = channel.getFormatSupplier().getStandardFormat()
+                .replace("{name}", channel.getName())
+                .replace("{nick}", channel.getNick() + "|" + fromServer)
+                .replace("{color}", channel.getColor().toString())
+                .replace("{msg}", message)
+                .replace("{sender}", VaultUtil.getPlayerName(sender))
+                .replace("{plainsender}", sender)
+                .replace("{world}", world)
+                .replace("{prefix}", VaultUtil.getPlayerPrefix(sender))
+                .replace("{suffix}", VaultUtil.getPlayerSuffix(sender))
+                .replace("{group}", VaultUtil.getGroup(sender))
+                .replace("{groupprefix}", VaultUtil.getGroupPrefix(sender))
+                .replace("{groupsuffix}", VaultUtil.getGroupSuffix(sender));
         return ChatColor.translateAlternateColorCodes('&', formatted);
     }
 
